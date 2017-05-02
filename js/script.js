8ac6dee3c1e64ef2143af05068b541265b663007
Luis Dunkum <luis.silvan365@gmail.com>
2017-05-02 10:54:47 +0100

    added external loading and fanciness

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
+let alreadyClickedOnce = false;
 const startLearning = function(){
-    $('#play-button').addClass('rotating-play-button');
-    $('#start-learning-link').html('Yea');
-    
+    if(!alreadyClickedOnce){
+        $('#play-button').addClass('rotating-play-button');
+        $('#start-learning-link').html('Yea');
+        alreadyClickedOnce = true;
+    }else{
+        $.ajax({
+            context: this,
+            dataType : "html",
+            url : "html/learning.html",
+            success : function(results) {
+                $('#content').html(results);
+                $('head').append('<link rel="stylesheet" type="text/css" href="css/learning.css">');
+            }
+        });
+    }
 };
