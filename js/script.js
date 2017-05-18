095b57ebb2c0dd848abeac33333ca070bf8da8b5
lDunkum <luis.silvan365@gmail.com>
2017-05-18 20:22:53 +0100

    html subfolger added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
+let alreadyClickedOnce = false;
 const startLearning = function(){
-    $('#play-button').addClass('rotating-play-button');
-    $('#start-learning-link').html('Yea');
-    
+    if(!alreadyClickedOnce){
+        $('#play-button').addClass('rotating-play-button');
+        $('#start-learning-link').html('Yea');
+        alreadyClickedOnce = true;
+    }else{
+        console.log("already clicked once");
+        $.ajax({
+            context: this,
+            dataType : "html",
+            url : "html/learning.html",
+            success : function(results) {
+                $('#content').html(results);
+            }
+        });
+    }
 };
